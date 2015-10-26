268a59b97b559d31bbd2ebb95318e91440608364
joksina <jksalaudeen@yahoo.com>
2015-10-26 14:48:00 -0700

    adding element

---
+++ linkedlist.js
@@ -0,0 +1,26 @@
+function linkedList(){
+	//this is the helper class
+	//the element is the value we want to add to the list
+	var Node = function(element){
+		this.element = element;
+		this.next = null;
+	};
+	// stores the length of the items in list
+	var length = 0;
+	var head = null;
+	//this adds a new item to the list
+	this.append = function(){};
+	//this inserts to a specified position
+	this.insert = function(position, element){};
+	
+	this.removeAt = function(){};
+	
+	this.remove = function(){};
+	//this returns the index of element
+	this.indexOf = function(){};
+	//returns true if the linked list is empty
+	this.isEmpty = function(){};
+	this.size = function(){};
+	this.toString = function(){};
+	this.print = function(){};
+}
